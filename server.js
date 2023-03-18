434581ec171922e98ebf6ade606b23be86a95159
nirgluzman <nir.gluzman@gmail.com>
2023-03-18 11:40:33 +0100

    add basic .ejs templates for login and register pages

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.set("view-engine", "ejs");
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Login server !");
+  res.render("index.ejs", { name: "Nir" });
 });
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`.red);
