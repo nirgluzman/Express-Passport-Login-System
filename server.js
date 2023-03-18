91e2e989ab308503a6ac4904b9231b2a8fa2b4a8
nirgluzman <nir.gluzman@gmail.com>
2023-03-18 15:52:21 +0100

    full integration with Passport authentication middleware

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 require("colors");
 
 const express = require("express");
+const methodOverride = require("method-override");
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
 
 const PORT = process.env.PORT || 3000;
+
+const initializePassport = require("./passport-config");
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
 // local variable to store the users database
 const users = [];
@@ -13,36 +28,78 @@ const app = express();
 app.set("view-engine", "ejs");
 
 app.use(express.urlencoded({ extended: false })); // to access the form info inside .ejs
-app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "Nir" });
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
 });
 
-app.get("/login", (req, res) => {
+app.get("/login", checkNotAuthenticated, (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {});
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-app.get("/register", (req, res) => {
+app.get("/register", checkNotAuthenticated, (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {
+app.post("/register", checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
-      id: Date.now(),
+      id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword,
     });
     res.redirect("/login");
-  } catch (err) {
+  } catch {
     console.log(err);
     res.redirect("/register");
   }
+});
+
+app.delete("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
 });
 
 app.listen(PORT, () => {
