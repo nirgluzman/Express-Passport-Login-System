057ca9fc35ad448956e9e2c0611d3deb690f4de5
nirgluzman <nir.gluzman@gmail.com>
2023-03-18 11:51:44 +0100

    add POST for register

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 require("colors");
 
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const PORT = process.env.PORT || 3000;
+
+// local variable to store the users database
+const users = [];
 
 const app = express();
 
 app.set("view-engine", "ejs");
+
+app.use(express.urlencoded({ extended: false })); // to access the form info inside .ejs
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -17,11 +23,27 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
+app.post("/login", (req, res) => {});
+
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", (req, res) => {});
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/register");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`.red);
